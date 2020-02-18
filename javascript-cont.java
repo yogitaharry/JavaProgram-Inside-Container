c3c83e339a699da3947674658dd7b4b60031ca51
Harryogita <52612145+Harryogita@users.noreply.github.com>
2020-02-18 18:19:25 +0530

    Create javascript-cont.java

---
+++ javascript-cont.java
@@ -0,0 +1,18 @@
+/* This is a simple Java program.
+   FileName : "javascript-cont.java". */
+
+
+
+class javascript-cont
+{
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+        System.out.println("This is Harry , Its java program running inside container");
+        System.out.println("Same like this you can build/run your program inside container and get expected output as per your need");
+
+
+    }
+}
