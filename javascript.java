1ddf710c6d60895ca81d163c99593c403f0ea670
Harryogita <52612145+Harryogita@users.noreply.github.com>
2020-02-18 18:26:53 +0530

    Create javascript.java

---
+++ javascript.java
@@ -0,0 +1,18 @@
+/* This is a simple Java program.
+   FileName : "javascript.java". */
+
+
+
+class javascript
+{
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+        System.out.println("This is Harry , Its java program running inside container");
+        System.out.println("Same like this you can build/run your program inside container and get expected output as per your need");
+
+
+    }
+}
